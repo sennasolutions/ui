3a37385191d4ff7ef506e5b2736e96c33fcd60a4
Lennard <lennard@spring.nl>
2022-01-14 12:13:33 +0100

    wip

--- resources/views/components/avatar/circle.blade.php
+++ resources/views/components/avatar/circle.blade.php
@@ -34,7 +34,7 @@
 @if($img && !str_contains($img, "ui-avatars.com"))
 <img {{ $attributes->merge([
     'src' => $img,
-    'class' => 'flex-grow-0 flex-shrink-0  rounded-full object-cover ' . $sizeClass,
+    'class' => 'grow-0 shrink-0  rounded-full object-cover ' . $sizeClass,
     'alt' => $name
     ]) }} />
 @else
@@ -52,7 +52,7 @@
 
 
 
-<svg {{ $attributes->merge(['class' => $sizeClass . ' flex-grow-0 flex-shrink-0  rounded-full bg-white']) }} xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 64 64" version="1.1">
+<svg {{ $attributes->merge(['class' => $sizeClass . ' grow-0 shrink-0  rounded-full bg-white']) }} xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 64 64" version="1.1">
     <rect fill="{{ $bgColor }}" cx="32" width="64" height="64" cy="32" r="32"/>
     <text x="50%" y="50%"
     style="color: {{ $textColor }}; line-height: 1;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;"
