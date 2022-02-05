014e50a974c0548503ed14c106fea3ec9b14bed9
Lennard <lennard@spring.nl>
2022-02-05 12:44:35 +0100

    media

--- resources/views/components/modal/basic.blade.php
+++ resources/views/components/modal/basic.blade.php
@@ -23,8 +23,8 @@ $maxWidth = [
     x-on:keydown.tab.prevent="$event.shiftKey || nextFocusable().focus()"
     x-on:keydown.shift.tab.prevent="prevFocusable().focus()"
     x-show="showMe"
-    {{-- id="{{ $id }}" --}}
-    class="sn-modal fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50"
+    class="sn-modal fixed inset-0 overflow-y-auto px-4 py-0 sm:px-0 z-50"
+    {{ $attributes->only('wire:key') }}
     style="display: none;"
 >
     @if($backdrop)
