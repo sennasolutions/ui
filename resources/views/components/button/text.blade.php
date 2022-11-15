c8f739fec78d22ad2f25637de7daec7c0376ce78
Lennard <lennard@spring.nl>
2022-11-15 14:33:12 +0100

    wip

--- resources/views/components/button/text.blade.php
+++ resources/views/components/button/text.blade.php
@@ -1,28 +1,3 @@
-{{-- @php
-/**
- * @name Text Button
- * @description A text button
- */
-@endphp
-
-@props([
-    /**
-     * @param string tag Tag that is used for this button. For example 'a' or 'button'. default: 'button'
-     */
-    'tag' => 'button',
-    /**
-     * @param string colorClass String of color classes applied to the button. default: text-primary
-     */
-    'colorClass' => 'text-primary',
-    /**
-     * @param string type Type that is used for this button. For example 'submit' or 'button'. default: 'button'
-     */
-    'type' => 'button'
-])
-
-<{{ $tag }} type="{{ $type }}" {{ $attributes->merge(['class' => "sn-button-text font-semibold $colorClass hover:underline" ]) }}>
-    {{ $slot }}
-</{{ $tag }}> --}}
 <x-senna.button {{ $attributes->merge(['class' => 'text-primary focus:outline-none focus:ring focus:ring-primary-300 focus:ring-offset-2 ']) }} :textButton="true" colorClass="">
 {{ $slot }}
 </x-senna.button>
