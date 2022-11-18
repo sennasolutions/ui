9524085c690757df862629c7d6447d944fd8ea9b
Lennard <lennard@spring.nl>
2022-11-18 14:21:00 +0100

    x

--- resources/views/components/input/filter-select-item.blade.php
+++ resources/views/components/input/filter-select-item.blade.php
@@ -40,7 +40,7 @@ $id = str($name)->slug() . str($label)->slug() . $key;
         @if($allowHtml){!! $label !!}@else{{ $label }}@endif
     </label>
     @if($showDeleteButton)
-    <svg x-on:click="deleteValue('{{ $key }}')" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-auto cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <svg x-on:click="deleteValue('{{ $key }}')" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-auto cursor-pointer flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
     @endif
