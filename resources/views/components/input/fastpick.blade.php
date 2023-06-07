6c2acec1ee330e25b6ae8a2f78454f62ba276103
Lennard <168357+lennardv2@users.noreply.github.com>
2023-06-07 15:31:53 +0200

    wip

--- resources/views/components/input/fastpick.blade.php
+++ resources/views/components/input/fastpick.blade.php
@@ -28,7 +28,7 @@
 }">
     <x-senna.dropdown {{ $attributes->namespace('dropdown')->merge(['align' => 'left']) }}>
         <x-slot name="trigger">
-            <div {{ $attributes->namespace('trigger')->merge(['class' => "text-primary inline-flex gap-1 items-center justify-center"]) }} x-show="options.length > 1" class="">
+            <div {{ $attributes->namespace('trigger')->merge(['class' => "cursor-pointer text-primary inline-flex gap-1 items-center justify-center"]) }} x-show="options.length > 1" class="">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" {{ $attributes->namespace('icon')->merge(['class' => "w-3 h-3"]) }}>
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                 </svg>
