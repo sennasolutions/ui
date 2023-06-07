2b05bb384ffa35ee24bce507cdc497c5f3940fa2
Lennard <168357+lennardv2@users.noreply.github.com>
2023-06-07 15:24:13 +0200

    wip

--- resources/views/components/input/fastpick.blade.php
+++ resources/views/components/input/fastpick.blade.php
@@ -28,15 +28,17 @@
 }">
     <x-senna.dropdown {{ $attributes->namespace('dropdown')->merge(['align' => 'left']) }}>
         <x-slot name="trigger">
-            <x-senna.button.text x-show="options.length > 1" iconClass="w-5 h-5 text-primary" class="text-current gap-0" iconPosition="right" icon="hs-chevron-down">
+            <div {{ $attributes->namespace('trigger')->merge(['class' => "text-primary inline-flex gap-1 items-center justify-center"]) }} x-show="options.length > 1" class="">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" {{ $attributes->namespace('icon')->merge(['class' => "w-3 h-3"]) }}>
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                </svg>
                 <span x-text="valueText"></span>
-            </x-senna.button.text>
-            <x-senna.button.text x-show="options.length === 1" class="text-current !font-normal gap-0">
+            </div>
+            <div {{ $attributes->namespace('trigger')->merge(['class' => "text-primary inline-flex gap-1 items-center justify-center"]) }} x-show="options.length === 1" class="">
                 <span x-text="valueText"></span>
-            </x-senna.button.text>
+            </div>
         </x-slot>
         <x-slot name="content" class="flex flex-col gap-2">
-            
             <template x-for="option in options" v-bind:key="option.value">
                 <x-senna.dropdown.item {{ $attributes->namespace('item') }} x-on:click="value = option.value">
                     <span x-text="option.label"></span>
