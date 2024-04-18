505987bba19991b365a2664f0bca7b31a9ad119c
Lennard <168357+lennardv2@users.noreply.github.com>
2024-04-18 15:57:07 +0200

    Fix

--- resources/views/components/google-maps.blade.php
+++ resources/views/components/google-maps.blade.php
@@ -1,178 +1,183 @@
 @php
 /**
- * @name Maps
- * @description Location input with an optional google maps display. Be sure to set maps.apiKey in config
- */
+* @name Maps
+* @description Location input with an optional google maps display. Be sure to set maps.apiKey in config
+*/
 @endphp
 
 @props([
-     /**
-     * @param array config The google maps config.
-     */
-    'config' => [
-        'zoom' => 2,
-        'center' => [
-            'lat' => 6.20,
-            'lng' => -49.5,
-        ],
-        'fullscreenControl' => false,
-        'mapTypeControl' => false,
-        'streetViewControl' => false,
-        'zoomControl' => true
-
+/**
+* @param array config The google maps config.
+*/
+'config' => [
+    'zoom' => 2,
+    'center' => [
+    'lat' => 6.20,
+    'lng' => -49.5,
     ],
-    'markers' => [
-        [
-            'content' => 'hey',
-            'position' => [
-                'lat' => 51.2,
-                'lng' => 5.4,
-            ]
-        ]
-    ]
+    'fullscreenControl' => false,
+    'mapTypeControl' => false,
+    'streetViewControl' => false,
+    'zoomControl' => true
+
+],
+'markers' => [
+[
+'title' => 'hey',
+'position' => [
+'lat' => 51.2,
+'lng' => 5.4,
+]
+]
+]
 ])
 
 @wireProps
 
 @if(!config('senna.ui.maps.apiKey'))
-    <div>
-        {{ __('Please set a google API Key in the config file (maps.apiKey)') }}
-    </div>
+<div>
+    {{ __('Please set a google API Key in the config file (maps.apiKey)') }}
+</div>
 @else
 
-<div 
-    data-sn="google-maps" 
-    {{ $attributes->root() }}
-    wire:ignore 
-    x-data="{
-        config: @entangleProp('config'),
-        markers: @entangleProp('markers'),
-        $map: null,
-        instance: null,
-        instanceMarkers: [],
-        {{-- maxZoom:  @entangleProp('maxZoom'), --}}
-        infoWindow: null,
-        init() {
-            if (typeof Alpine.mapComponents === 'undefined') {
-                Alpine.mapComponents = [];
-            }
-            this.$map = this.$refs.map
-
-            if (window.mapIsLoaded) {
-                this.onReady();
-            }
-
-            Alpine.mapComponents.push(this);
-
-            this.$watch('markers', () => this.updateMarkers());
-            this.$watch('config', () => this.updateConfig());
-        },
-        onReady() {
-            if (this.$map) {
-                {{-- console.log(this.config) --}}
-                this.instance = new google.maps.Map(this.$map, {...this.config});
-                {{-- this.instance.addListener('zoom_changed', () => {
-                    if (this.instance.getZoom() > this.maxZoom) {
-                        this.instance.setZoom(this.maxZoom);
-                    }
-                }); --}}
-
-                {{-- console.log(this.markers); --}}
-                this.infoWindow = new google.maps.InfoWindow;
-
-                this.instance.addListener('click', (event) => {
-                    this.infoWindow.close();
-                });
-            }
-
-            this.updateMarkers()
-        },
-        updateConfig() {
-            if (this.instance) {
-                this.instance.setOptions(this.config);
-            }
-        },
-        updateMarkers() {
-            this.instanceMarkers.forEach(marker => marker.setMap(null));
-            this.instanceMarkers = [];
-
-            this.markers.forEach(marker => {
-                // Set a popup window that closes when clicked outside the marker
-                let gmarker = new google.maps.Marker({
-                    ...marker,
-                    map: this.instance,
-                })
-
-                if (marker.content) {
-                    gmarker.addListener('click', () => {
-                        this.infoWindow.setContent(marker.content);
-                        this.infoWindow.open(this.instance, gmarker);
-                    });
-                }
-
-                this.instanceMarkers.push(gmarker);
-            });
-            
-            this.fitMarkers();
-        },
-        fitMarkers() {
-            if (!this.markers.length) return;
-
-
-            const bounds = new google.maps.LatLngBounds();
-            this.instanceMarkers.forEach(marker => bounds.extend(marker.getPosition()));
-            // fitbounds with padding
-
-            this.instance.fitBounds(bounds, 50);
-            var zoom = this.instance.getZoom();
-      
-            google.maps.event.addListenerOnce(this.instance, 'bounds_changed', function() { 
-                this.setZoom(Math.min(12, this.getZoom())); 
-            });
-
-            @wireMethod('onFitMarkers')(this.markers);
-        }
-    }">
-        <div wire:ignore x-ref="map" class="sn-google-maps" {{ $attributes->namespace('map') }} ></div>
+<div data-sn="google-maps" {{ $attributes->root() }}
+    wire:ignore
+    x-data="google_maps({
+    config: @entangleProp('config'),
+    markers: @entangleProp('markers')
+    })">
+    <div wire:ignore x-ref="map" class="sn-google-maps" {{ $attributes->namespace('map') }} ></div>
 
 </div>
 
 @once
-    @push('senna-ui-styles')
-    <style type="text/css">
-        .sn-google-maps {
-            height: 400px;
-            width: 100%;
-        }
-    </style>
+@push('senna-ui-styles')
+<style type="text/css">
+    .sn-google-maps {
+        height: 400px;
+        width: 100%;
+    }
+</style>
 
-    @endpush
-    @push('senna-ui-scripts')
-    <script>
-      window.mapIsLoaded = false;
+@endpush
+@push('senna-ui-scripts')
+<script>
+    (g => { var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document, b = window; b = b[c] || (b[c] = {}); var d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams, u = () => h || (h = new Promise(async (f, n) => { await (a = m.createElement("script")); e.set("libraries", [...r] + ""); for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]); e.set("callback", c + ".maps." + q); a.src = `https://maps.${c}apis.com/maps/api/js?` + e; d[q] = f; a.onerror = () => h = n(Error(p + " could not load.")); a.nonce = m.querySelector("script[nonce]")?.nonce || ""; m.head.append(a) })); d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)) })({
+        key: "{{ config('senna.ui.maps.apiKey') }}",
+        v: "weekly",
+        // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+        // Add other bootstrap parameters as needed, using camel case.
+    });
 
-      function googleMapsInit() {
-        window.mapIsLoaded = true;
+</script>
 
-        // If alpine was first with loading
-        if (typeof Alpine == 'undefined') return;
+<script>
+    document.addEventListener('alpine:init', () => {
+        /**
+        This creates the conditional field options
+        */
+        Alpine.data('google_maps', ({ config, markers }) => {
+            let googleMap = null
+            let infoWindow = null
+            let googleMapMarkers = []
+        
+            return {
+                config,
+                markers,
+                $map: null,
+                instance: null,
+                infoWindow: null,
+                google: {},
+                async init() {
+                    this.google.maps = await google.maps.importLibrary("maps");
+                    this.google.marker = await google.maps.importLibrary("marker");
 
-        if (typeof Alpine.mapComponents === 'undefined') {
-            Alpine.mapComponents = [];
-        }
+                    this.$map = this.$refs.map
 
-        Alpine.mapComponents.forEach(c => {
-            c.onReady();
+                    this.initMap();
+                    await this.$watch('markers', async () => await this.updateMarkers());
+                    this.$watch('config', () => this.updateConfig());
+                },
+                async initMap() {
+                    const { Map } = await google.maps.importLibrary("maps")
+                    const { InfoWindow } = await google.maps.importLibrary("maps");
+                  
+                    // Request needed libraries.
+                    googleMap = new Map(this.$map, {
+                        center: { lat: 37.4239163, lng: -122.0947209 },
+                        zoom: 14,
+                        mapId: Math.random().toString(36).substring(7),
+                    });
+
+                    infoWindow = new InfoWindow();
+
+                    // on click close info window
+                    googleMap.addListener('click', () => {
+                        if (infoWindow) {
+                            infoWindow.close();
+                        }
+                    });
+               
+                    await this.updateMarkers()
+                },
+                updateConfig() {
+                    if (this.instance) {
+                        this.instance.setOptions(this.config);
+                    }
+                },
+                async updateMarkers() {
+                    const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary('marker');
+                    
+                    googleMapMarkers.forEach(marker => marker.setMap(null));
+                    googleMapMarkers = [];
+
+                    this.markers.forEach(({position, title, popup}, i) => {
+                        // Set a popup window that closes when clicked outside the marker
+                        const pin = new PinElement({
+                            // glyph: `${i + 1}`,
+                        });
+
+                        let gmarker = new AdvancedMarkerElement({
+                            map: googleMap,
+                            position,
+                            title: `${i + 1}. ${title}`,
+                            content: pin.element
+                        });
+
+                        if (popup) {
+                            gmarker.addListener('click', () => {
+                                infoWindow.close();
+                                infoWindow.setContent(popup);
+                                infoWindow.open(googleMap, gmarker);
+                            });
+                        }
+
+                        googleMapMarkers.push(gmarker);
+                    });
+
+                    this.fitMarkers();
+                },
+                fitMarkers() {
+                    if (!this.markers.length) return;
+
+                    const bounds = new google.maps.LatLngBounds();
+                    googleMapMarkers.forEach(marker => bounds.extend(marker.position));
+                    // fitbounds with padding
+                    googleMap.fitBounds(bounds, 50);
+                    var zoom = googleMap.getZoom();
+
+                    google.maps.event.addListenerOnce(googleMap, 'bounds_changed', function () {
+                        this.setZoom(Math.min(12, this.getZoom()));
+                    });
+
+                    @wireMethod('onFitMarkers')(this.markers);
+                }
+            }
         })
-      }
-    </script>
+    });
+</script>
 
-    <script
-        src="https://maps.googleapis.com/maps/api/js?key={{ config('senna.ui.maps.apiKey') }}&callback=googleMapsInit&libraries=places&v=weekly"
-        async>
-    </script>
-
-    @endpush
+@endpush
 @endonce
 
 @endif
