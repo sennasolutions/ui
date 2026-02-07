a41ec540955fbf636947f867e7e08e86366770d6
Lennard <168357+lennardv2@users.noreply.github.com>
2026-02-07 22:29:06 +0000

    wip

--- resources/views/components/editor/tinymce-media.blade.php
+++ resources/views/components/editor/tinymce-media.blade.php
@@ -38,10 +38,19 @@
                             videoId = url.split('youtu.be/')[1].split('?')[0].split(' ')[0];
                         } else if (url.indexOf('v=') !== -1) {
                             videoId = url.split('v=')[1].split('&')[0].split(' ')[0];
+                        } else if (url.indexOf('/shorts/') !== -1) {
+                            videoId = url.split('/shorts/')[1].split('?')[0].split(' ')[0];
                         } else if (url.indexOf('/embed/') !== -1) {
                             videoId = url.split('/embed/')[1].split('?')[0].split(' ')[0];
                         }
                         return videoId ? 'https://www.youtube.com/embed/' + videoId : url;
+                    },
+                    getVimeoEmbedUrl(url) {
+                        var match = url.match(/vimeo\.com\/(\d+)/);
+                        return match ? 'https://player.vimeo.com/video/' + match[1] : url;
+                    },
+                    buildEmbedIframe(src) {
+                        return '<iframe src="' + src + '" style="width:100%; aspect-ratio:16/9;" frameborder="0" allowfullscreen></iframe>';
                     },
                     init() {
                         let component = this;
@@ -58,7 +67,8 @@
                             media_live_embeds: true,
                             block_formats: 'Paragraaf=p; Kop=h3',
                             toolbar: 'undo redo | formatselect | bold italic | bullist numlist | removeformat link | insertmedia | code',
-                            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:15px } img { max-width: 100%; height: auto; } span.mce-preview-object[data-mce-object="iframe"] { display: inline-block; max-width: 100% !important; height: auto !important; aspect-ratio: 16/9; overflow: hidden; } span.mce-preview-object[data-mce-object="iframe"] iframe { display: block; width: 100%; height: 100%; }',
+                            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:15px } img { max-width: 100%; height: auto; } span.mce-preview-object[data-mce-object="iframe"] { display: block; width: 360px; max-width: 100%; aspect-ratio: 16/9; overflow: hidden; } span.mce-preview-object[data-mce-object="iframe"] iframe { display: block; width: 100%; height: 100%; }',
+                            object_resizing: 'img',
                             paste_block_drop: true,
                             link_default_target: '_blank',
                             link_assume_external_targets: true,
@@ -88,6 +98,30 @@
                                     }
                                 });
 
+                                editor.on('PastePreProcess', (e) => {
+                                    var tmp = document.createElement('div');
+                                    tmp.innerHTML = e.content;
+                                    var text = (tmp.textContent || tmp.innerText || '').trim();
+
+                                    // YouTube
+                                    if (/^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|shorts\/|embed\/)|youtu\.be\/)/.test(text)) {
+                                        var embedUrl = component.getYouTubeEmbedUrl(text);
+                                        if (embedUrl !== text) {
+                                            e.content = component.buildEmbedIframe(embedUrl);
+                                            return;
+                                        }
+                                    }
+
+                                    // Vimeo
+                                    if (/^(?:https?:\/\/)?(?:www\.)?vimeo\.com\/\d+/.test(text)) {
+                                        var embedUrl = component.getVimeoEmbedUrl(text);
+                                        if (embedUrl !== text) {
+                                            e.content = component.buildEmbedIframe(embedUrl);
+                                            return;
+                                        }
+                                    }
+                                });
+
                                 editor.on('blur', () => {
                                     component.currentValue = editor.getContent();
                                 });
@@ -99,7 +133,7 @@
                                 var html = '';
                                 if (type === 'video-link') {
                                     var embedUrl = component.getYouTubeEmbedUrl(url);
-                                    html = '<iframe src="' + embedUrl + '" width="560" height="315" style="max-width:100%; height:auto; aspect-ratio:16/9;" frameborder="0" allowfullscreen></iframe>';
+                                    html = component.buildEmbedIframe(embedUrl);
                                 } else {
                                     html = '<img src="' + url + '" alt="" style="max-width:100%; height:auto;" />';
                                 }
